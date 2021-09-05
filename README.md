d26cbbfe55aeaab271e0c9b246ed0c20f92e7264
gamefang <337503110@qq.com>
2021-09-05 14:45:51 +0800

    添加匈奴世系表，整理文件夾結構
    Former-commit-id: 0a724834b3830241d5d8e76983531758684ae826

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# zizhitongjian
-资治通鉴笔记、整理、数据化等内容
+# 資治通鑑
+
+> 筆記、整理、數據化
+
+> 制作：[gamefang](https://gamefang.github.io/)
+
+---
+
+[<點擊進入>](http://gamefang.fun/zizhitongjian/)
+
+
+- 個人讀書筆記，不考慮準確性與全面性
+- 使用工具
+    - [markdown preview enhanced](https://atom.io/packages/markdown-preview-enhanced)：Atom插件，markdown轉html
+    - [csvtotable](https://pypi.org/project/csvtotable/)：Python擴展包，csv轉html
--- create.bat
+++ create.bat
@@ -1,11 +1,12 @@
 @echo off
 REM pip install csvtotable
 
-REM 1
-cd 主要數據
+REM 1 -------------------
+cd 1_主要數據
+
 csvtotable 通鑒目錄.csv 通鑒目錄.html -o -c 通鑒目錄 -e -dl 100
 csvtotable 人物總表.csv 人物總表.html -o -c 人物總表 -vs -1 -e -dl 100
-csvtotable 事件表.csv 事件表.html -o -c 事件表 -e -dl 100
+REM csvtotable 事件表.csv 事件表.html -o -c 事件表 -e -dl 100
 
 cd 政權年代表
 csvtotable 戰國秦.csv 戰國秦.html -o -c 戰國秦 -e
@@ -13,46 +14,38 @@ csvtotable 漢.csv 漢.html -o -c 漢 -e
 cd ..
 
 cd ..
+REM -------------------
 
-REM 2
-cd 年代專項
+REM 2 -------------------
+cd 2_世系圖表
 
-REM cd 秦末楚漢
-REM csvtotable 秦末楚漢政權.csv 秦末楚漢政權.html -o -c 秦末楚漢政權 -e
+REM cd 2.1_中國
 REM cd ..
 
-cd 西漢
-REM csvtotable 西漢開國功臣列表.csv 西漢開國功臣列表.html -o -c 西漢開國功臣列表 -e
-REM csvtotable 張騫通西域諸國概況.csv 張騫通西域諸國概況.html -o -c 張騫通西域諸國概況 -e
+cd 2.2_四夷
+csvtotable 匈奴世系表.csv 匈奴世系表.html -o -c 匈奴世系表 -e
 cd ..
 
-cd 新朝
-REM csvtotable 王莽寶貨制.csv 王莽寶貨制.html -o -c 王莽寶貨制 -e
-cd ..
+REM cd 2.9_雜項
+REM cd ..
 
-cd 東漢
+cd ..
+REM -------------------
+
+REM 3 -------------------
+cd 3_年代專項
+
+cd 3.5_東漢
 csvtotable 云臺二十八將表.csv 云臺二十八將表.html -o -c 云臺二十八將表 -e
 cd ..
 
 cd ..
+REM -------------------
 
-REM 3
-cd 通用專項
-
-cd 歷代世系圖
-REM csvtotable 西漢世系表.csv 西漢世系表.html -o -c 西漢世系表 -e
-cd ..
-
-cd 雜項
-REM csvtotable 干支表.csv 干支表.html -o -c 干支表 -e
-REM csvtotable 攝提對照表.csv 攝提對照表.html -o -c 攝提對照表 -e
+REM 4 -------------------
+cd 4_通用專項
 csvtotable 和親列表.csv 和親列表.html -o -c 和親列表 -e
-REM csvtotable 被詛咒的趙王.csv 被詛咒的趙王.html -o -c 被詛咒的趙王 -e
-REM csvtotable 百越各國.csv 百越各國.html -o -c 百越各國 -e
-REM csvtotable 匈奴語對照表.csv 匈奴語對照表.html -o -c 匈奴語對照表 -e
 cd ..
-
-cd ..
-
+REM -------------------
 
 pause
